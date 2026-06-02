b851c68470d2235ad6a051dc0b7c780923a8019d
gelianmtz <ger.elian.martinez@outlook.com>
2026-06-02 11:07:25 -0700

    add dynamic pages

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -29,14 +29,21 @@ def get_request(endpoint, **kwargs):
         print("Network exception occurred")
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
+
+    print("Sentiment URL:", request_url)
+
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
+
+        print("Status:", response.status_code)
+        print("Body:", response.text)
+
         return response.json()
+
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
-        print("Network exception occurred")
+        return None
 
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
